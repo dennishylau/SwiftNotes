bba2a42081e4dece6f9226c986e61023a7f04960
Dennis Lau <dennishylau@gmail.com>
2017-09-29 00:56:30 +0800

    20170929 0056

--- Pages/Notes P07 Class.xcplaygroundpage/Contents.swift
+++ Pages/Notes P07 Class.xcplaygroundpage/Contents.swift
@@ -43,10 +43,11 @@ class Train: Vehicle {
 let train1 = Train()
 train1.makeNoise()
 //: ## Superclass
+// override the superclass's property
 class Car: Vehicle {
 	var gear = 1
 	override var description: String {
-		return super.description + "in gear \(gear)"
+		return super.description + "in gear \(gear)"	// use the super keyword for a superclass's property
 	}
 }
 
@@ -54,6 +55,45 @@ let car1 = Car()
 car1.currentSpeed = 50
 car1.gear = 5
 print("Car \(car1.description).")
+//: ## Initializer
+// Unlike structs, memberwise initializers are not automatically created for classes
+// There are two ways to do this. (1) Set default value for everything or (2) just write it manually
+class Person {
+	var name: String
+	
+	init(name: String) {
+		self.name = name
+	}
+}
+var person1 = Person(name: "Dennis Lau")
+
+class Student:Person {
+	var favoriteSubject: String
+	init(name:String,favoriteSubject:String) {
+		self.favoriteSubject = favoriteSubject
+		super.init(name: name)	// only call super on superclass initializer after initializing all subclass porperties
+	}
+}
+//: ## Value Referencing
+// A copied class references the same memory location as the original class
+// All class constants and variables have stable identity, meaning that within all copies of the class they all point to the same data
+var person2 = Person(name: "Godfrey")
+var myFriend = person2
+print(person2.name)					// person2 and myFriend point to the same memory location
+print(myFriend.name)
+myFriend.name = "Adrian"
+print(person2.name)					// change one name, change all names
+print(myFriend.name)
+myFriend = Person(name: "Ken")
+print(person2.name)					// now myFriend is separate from person2
+print(myFriend.name)
+// struct works the opposite way, a copied struct always uses a different memory location, no referencing
+//: - Note:\
+//:Default to using a struct unless a class is necessary\
+//:1\. Class required as pass in value\
+//:2\. Inheritance of methods and properties\
+//:3\. Referencing
+
 
 
 
